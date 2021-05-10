6b326096b93eff444121c2729fca412fa32ee607
Mateus Santos <mateus.sa94@hotmail.com>
2021-05-10 12:02:01 -0300

    atualizei o arquivo index.html e criei o arquivo script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+let count = 0;
+//select values and buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function(btn){
+	btn.addEventListener("click", function(e){
+		const styles = e.currentTarget.classList;
+		if(styles.contains("decrease")){
+			count --;
+		}
+		
+		if(count > 0){
+			value.style.color = "green";
+		}
+		value.textContent = count;
+	});
+});
